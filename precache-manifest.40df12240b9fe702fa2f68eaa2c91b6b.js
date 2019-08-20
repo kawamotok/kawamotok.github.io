b1cc4230491c6ef19599f63da30f73ddec3dc3f7
川本孝一 <kawamoto_kouichi@sgk.co.jp>
2019-08-20 18:30:28 +0900

    vuetstrainingを公開するテスト

---
+++ precache-manifest.40df12240b9fe702fa2f68eaa2c91b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "53bc47c0bcf299d20979",
+    "url": "/js/chunk-vendors.9446c683.js"
+  },
+  {
+    "revision": "ce28e7599b4290e3a0b7",
+    "url": "/js/app.85534ba3.js"
+  },
+  {
+    "revision": "edc01414044a73035eee7a1ac0f2c56d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ce28e7599b4290e3a0b7",
+    "url": "/css/app.b0448f85.css"
+  }
+];
